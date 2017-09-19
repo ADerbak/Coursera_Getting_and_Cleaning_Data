dfa37d98f973273178ab198f4a54cac1249db03d
ADerbak <andrewderbak@gmail.com>
2017-09-18 19:00:07 -0500

    second commit

---
+++ README - Analysis Notes.txt
@@ -0,0 +1,38 @@
+##Getting and Cleaning Data: Project
+
+#Introduction
+
+This repository contains my work for the course project for the Coursera course "Getting and Cleaning data", part of the Data Science specialization. What follows first are my notes on the original data.
+
+#About the raw data
+
+The features (561 of them) are unlabeled and can be found in the x_test.txt. The activity labels are in the y_test.txt file. The test subjects are in the subject_test.txt file.
+
+The same holds for the training set.
+
+#About the script and the tidy dataset
+
+I created a script called run_analysis.R which will merge the test and training sets together. Prerequisites for this script:
+
+the UCI HAR Dataset must be extracted and..
+the UCI HAR Dataset must be availble in a directory called "UCI HAR Dataset"
+After merging testing and training, labels are added and only columns that have to do with mean and standard deviation are kept.
+
+Lastly, the script will create a tidy data set containing the means of all the columns per test subject and per activity. This tidy dataset will be written to a tab-delimited file called tidy.txt, which can also be found in this repository.
+
+#About the Code Book
+
+The CodeBook file explains the transformations performed and the resulting data and variables.
+
+- Features are normalized and bounded within [-1,1].
+- Each feature vector is a row on the text file.
+
+License:
+========
+Use of this dataset in publications must be acknowledged by referencing the following publication [1] 
+
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+
+This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited.
+
+Jorge L. Reyes-Ortiz, Alessandro Ghio, Luca Oneto, Davide Anguita. November 2012.
---
+++ run_analysis.R
@@ -0,0 +1,89 @@
+## Coursera Getting and Cleaning Data Project. September 18th, 2017. A.Derbak
+
+# Load needed packages for tidying
+require(tidyr)
+require(stringr)
+require(dplyr)
+require(data.table)
+
+
+# Get data and set working directory
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",destfile = "wearable.zip")
+setwd("~\\R\\UCI HAR Dataset")
+
+# Get Features and activity labels
+
+features <- fread("features.txt")
+activity_labels <- fread("activity_labels.txt")
+
+# Read in the Test sets - Do not need Inertial Folder!!
+test_data <- read_table("test\\X_test.txt", col_names = FALSE)
+test_label <- fread("test\\y_test.txt")
+test_subject <- fread("test\\subject_test.txt")
+
+# Name the columns
+names(test_label) <- "activity"
+names(test_subject) <- "subjectID"
+test_data <- setnames(test_data, features$V2)
+
+
+# Combine all Test data
+test_all_data <- cbind(test_subject,test_label, test_data)
+
+
+
+# Read in the Training sets
+train_data <- read_table("train\\X_train.txt", col_names = FALSE)
+train_label <- fread("train\\y_train.txt")
+train_subject <- fread("train\\subject_train.txt")
+
+# Name the columns
+names(train_label) <- "activity"
+names(train_subject) <- "subjectID"
+train_data <- setnames(train_data, features$V2)
+
+# Combine all Training data
+train_all_data <- cbind(train_subject, train_label, train_data)
+
+
+
+# Combine Training and Test data sets
+total_data <- rbind(test_all_data,train_all_data)
+
+
+# Extract only the measurements on the mean and standard deviation
+# for each measurement.
+
+# determine which columns contain "mean()" or "std()"
+meanstdcols <- grepl("mean\\(\\)", names(total_data)) |
+  grepl("std\\(\\)", names(total_data))
+
+# ensure that we also keep the subjectID and activity columns
+meanstdcols[1:2] <- TRUE
+
+# remove unnecessary columns
+total_data_selected <- total_data[,meanstdcols, with=FALSE]
+
+
+#Get mean and StDev columns
+col_id <- grep("mean|std", total_data_selected)
+total_data_selected <- select(total_data_selected, col_id[1])
+
+
+# Use descriptive activity names to name the activities in the data set.
+# Appropriately label the data set with descriptive activity names.
+
+# convert the activity column from integer to factor
+total_data_selected$activity <- factor(total_data_selected$activity, labels=c("Walking",
+                                                        "Walking Upstairs", "Walking Downstairs", "Sitting", "Standing", "Laying"))
+
+
+# Creates an independent tidy data set with the AVG 
+# of each variable for each activity and each subject.
+
+# create the tidy data set
+melted_data <- melt(total_data_selected, id=c("subjectID","activity"))
+tidy_data <- dcast(melted_data, subjectID+activity ~ variable, mean)
+
+# write the tidy data set to a file
+write.csv(tidy_data, "tidy.csv", row.names=FALSE)
